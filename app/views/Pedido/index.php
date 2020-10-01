5375a1d6a1d2099ca275257580ec31897396227c
Roxy <roxipanaque@gmail.com>
2020-10-01 01:52:14 -0500

    XD

--- app/views/Pedido/index.php
+++ app/views/Pedido/index.php
@@ -6,7 +6,7 @@
   <title>Pedido</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
-
+  <!--ROXY-->
   <!-- Font Awesome -->
   <link rel="stylesheet" href="<?php echo RUTA_URL.'/public/plugins/fontawesome-free/css/all.min.css'?>">
   <!-- Ionicons -->

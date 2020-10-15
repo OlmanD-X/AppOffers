09d650b6265ee9f299267c891d02e4904e2f1f01
Priscila <priis_7_23@hotmail.com>
2020-10-14 22:22:29 -0500

    cambioscasifinal

--- app/controllers/Pedido.php
+++ app/controllers/Pedido.php
@@ -10,4 +10,75 @@
             header("location:../Login/index.php");
             $this->view(get_class($this).'/index'); //file 
         }
+        public function getAllPedidos()
+        {
+            $data = $this->modelPedido->mostrarTodosPedidos();
+            if(empty($data)){
+                throwError(GET_DATA_NOT_COMPLETE,'No existen registros');
+            }
+            else{
+                returnResponse(GET_REGISTIES_SUCCESSFULLY,'Se obtuvieron los registros exitosamente',$data);
+            }
+        }
+        public function getAllPedidosPersonalizados()
+        {
+            $data = $this->modelPedido->mostrarTodosPedidosPersonalizados();
+            if(empty($data)){
+                throwError(GET_DATA_NOT_COMPLETE,'No existen registros');
+            }
+            else{
+                returnResponse(GET_REGISTIES_SUCCESSFULLY,'Se obtuvieron los registros exitosamente',$data);
+            }
+        }
+
+        public function aceptar($idSolicitud)
+        {
+            $registyOk = $this->modelPedido->aceptarPedido($idSolicitud);
+            if($registyOk){
+                returnResponse(REGISTY_UPDATE_SUCCESSFULLY,'El estado del pedido fue actualizado con éxito');
+            }
+            else{
+                throwError(UPDATED_DATA_NOT_COMPLETE,'Se produjo un error al actualizar los datos');
+            }
+        }
+        public function aceptarPersonalizado($idSolicitud)
+        {
+            $registyOk = $this->modelPedido->aceptarPedidoPersonalizado($idSolicitud);
+            if($registyOk){
+                returnResponse(REGISTY_UPDATE_SUCCESSFULLY,'El estado del pedido fue actualizado con éxito');
+            }
+            else{
+                throwError(UPDATED_DATA_NOT_COMPLETE,'Se produjo un error al actualizar los datos');
+            }
+        }
+        public function rechazar($idSolicitud)
+        {
+            $registyOk = $this->modelPedido->rechazarPedido($idSolicitud);
+            if($registyOk){
+                returnResponse(REGISTY_UPDATE_SUCCESSFULLY,'El estado del pedido fue rechazado');
+            }
+            else{
+                throwError(UPDATED_DATA_NOT_COMPLETE,'Se produjo un error al actualizar los datos');
+            }
+        }
+        public function rechazarPersonalizado($idSolicitud)
+        {
+            $registyOk = $this->modelPedido->rechazarPedidoPersonalizado($idSolicitud);
+            if($registyOk){
+                returnResponse(REGISTY_UPDATE_SUCCESSFULLY,'El estado del pedido fue rechazado');
+            }
+            else{
+                throwError(UPDATED_DATA_NOT_COMPLETE,'Se produjo un error al actualizar los datos');
+            }
+        }
+        public function detallePedido($idSolicitud)
+        {
+            $data = $this->modelPedido->mostrarDetallePedido($idSolicitud);
+            if(empty($data)){
+                throwError(GET_DATA_NOT_COMPLETE,'No existen registros');
+            }
+            else{
+                returnResponse(GET_REGISTIES_SUCCESSFULLY,'Se obtuvieron los registros exitosamente',$data);
+            }
+        }
     }
--- app/models/ModelPedido.php
+++ app/models/ModelPedido.php
@@ -6,5 +6,40 @@
         public function __construct(){
             $this->db = new Connection;
         }
-
+        public function mostrarTodosPedidos(){
+            $this->db->query("EXECUTE SP_LeerSolicitudProducto");
+            $data = $this->db->getRegisties();
+            return $data;
+        }
+        public function mostrarTodosPedidosPersonalizados(){
+            $this->db->query("EXECUTE SP_LeerSolicitudPersonalizado");
+            $data = $this->db->getRegisties();
+            return $data;
+        }
+        public function aceptarPedido($idSolicitud){
+            $this->db->query("EXECUTE SP_AceptarPedido @id=:id");
+            $this->db->bind(':id',$idSolicitud);
+            return $this->db->execute();
+        }
+        public function aceptarPedidoPersonalizado($idSolicitud){
+            $this->db->query("EXECUTE SP_AceptarPedidoPersonalizado @id=:id");
+            $this->db->bind(':id',$idSolicitud);
+            return $this->db->execute();
+        }
+        public function rechazarPedido($idSolicitud){
+            $this->db->query("EXECUTE SP_RechazarPedido @id=:id");
+            $this->db->bind(':id',$idSolicitud);
+            return $this->db->execute();
+        }
+        public function rechazarPedidoPersonalizado($idSolicitud){
+            $this->db->query("EXECUTE SP_RechazarPedidoPersonalizado @id=:id");
+            $this->db->bind(':id',$idSolicitud);
+            return $this->db->execute();
+        }
+        public function mostrarDetallePedido($idSolicitud){
+            $this->db->query("EXECUTE SP_DetallePedido @id=:id");
+            $this->db->bind(':id',$idSolicitud);
+            $data = $this->db->getRegisties();
+            return $data;
+        }
     }
